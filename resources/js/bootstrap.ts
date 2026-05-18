e23a2c53df38ea4b2b20fb07e9cd8dbdf122faf5
sovouthea33 <sovouthea90@gmail.com>
2026-05-18 10:24:09 +0700

    feat: add laravel broadcasting support

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,21 @@
 import axios from 'axios';
-window.axios = axios;
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Pusher = Pusher;
+
+if (import.meta.env.VITE_PUSHER_APP_KEY) {
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+        wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+        wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+        wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+        forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+        enabledTransports: ['ws', 'wss'],
+    });
+}
